d3adb6cf520eec4a5c8be5b5e4f0d0d42ad58b90
Ben Ellis <dragonwolf@softweyr.co.uk>
2013-12-12 19:23:50 -0500

    Implemented low-level message parser for IRC RFC 2459.

--- src/bircd.js
+++ src/bircd.js
@@ -1,32 +1,156 @@
 var net = require('net');
 var commands = require('./commands.js');
 
+    var BIRCSession = function(socket) {
+        var self = this;
+        self.bufferSize = 512;
+        self.buffer = new Buffer(this.bufferSize);
+        self.size = 0;
+        
+        var getLastByteOnBuffer = function() {
+            if (this.size === 0) {
+                return null;
+            }
+            
+           var lastByteIndex = (this.position + this.size) % this.bufferSize;
+           return this.buffer[lastByteIndex];
+        };
+        
+        var appendChunkToBuffer = function(chunk) {
+            if (chunk.length + self.size > self.bufferSize) {
+                throw 'buffer overflow';
+            }
+            
+            chunk.copy(self.buffer, self.size);
+            self.size += chunk.length;
+        }
+        
+        var readToSpace = function(buffer, position) {
+            var index = position;
+            while (index < buffer.length && buffer[index] != 0x20) {
+                index++;
+            }
+            var endOfData = index;
+            
+            while (index < buffer.length && buffer[index] == 0x20) {
+                index++;
+            }
+            
+            return { buffer: buffer.slice(position, endOfData), index: index };
+        }
+        
+        var parseCommand = function(buffer)
+        {
+            var param;
+            var readResult;
+            var prefix = null;
+            var command = null;
+            var params = [];
+            var index = 0;
+            if (buffer[index] === 0x3A)
+            {
+                readResult = readToSpace(buffer, ++index);
+                prefix = readResult.buffer.toString('utf8');
+                index = readResult.index;
+            }
+            
+            readResult = readToSpace(buffer, index);
+            command = readResult.buffer.toString('utf8');
+            index = readResult.index;
+            
+            while (index !== buffer.length)
+            {
+                if (buffer[index] === 0x3A) {
+                    index++;
+                    param = buffer.slice(index, buffer.length);
+                    index = buffer.length;
+                }
+                else {
+                    readResult = readToSpace(buffer, index);
+                    param = readResult.buffer;
+                    index = readResult.index;
+                }
+                
+                params.push(param.toString('utf8'));
+            }
+            
+            return { prefix: prefix, command: command, params: params };
+        }
+        
+        var clearBuffer = function() {
+            self.size = 0;
+        };
+        
+        // Preamble
+        var clientId = socket.remoteAddress + ':' + socket.remotePort;
+          console.log(clientId + ' connected.');
+          socket.setTimeout(30000, function() {
+             console.log(clientId + ' timed out.');
+          });
+          socket.setKeepAlive(true, 0);
+          socket.on('end', function() {
+            console.log(clientId + ' disconnected.');
+          });
+          socket.on('data', function(chunk) {
+              // Check for CR LF
+              var startOfChunk = 0;
+              for (var i = 0; i < chunk.length; i++)
+              {
+                  if (chunk[i] === 0x0A) {
+                    if (i === 0) {
+                        // Check last byte on buffer
+                        if (getLastByteOnBuffer() == 0x0D)
+                        {
+                            // Concatenate with buffer and process command
+                            appendChunkToBuffer(chunk.slice(0, i));
+                            var command = parseCommand(self.buffer.slice(0, self.size - 2 + 1));
+                            console.log(command);
+                            clearBuffer();
+                            startOfChunk = i + 1
+                        }
+                        else
+                        {
+                            throw 'unexpected line feed character.';
+                        }
+                    }
+                    else
+                    {
+                        if (chunk[i-1] === 0x0D)
+                        {
+                            // Concatenate with buffer and process command
+                            appendChunkToBuffer(chunk.slice(0, i));
+                            var command = parseCommand(self.buffer.slice(0, self.size - 2 + 1));
+                            console.log(command);
+                            clearBuffer();
+                            startOfChunk = i + 1;
+                        }
+                        else
+                        {
+                            // TODO: ERROR!
+                            throw 'unexpected line feed character.';
+                        }
+                    }
+                  }
+              }
+              
+              if (startOfChunk < chunk.length) {
+                appendChunkToBuffer(chunk.slice(startOfChunk, chunk.length - startOfChunk));
+              }
+          });
+    };
+
     var BIRCDServer = function(options, connectionListener) {
+        var self = this;
+        self.sessions = [];
         var connectionListenerWrapper = function(c) {
             
-            // Preamble
-            var clientId = c.remoteAddress + ':' + c.remotePort;
-              console.log(clientId + ' connected.');
-              c.setTimeout(30000, function() {
-                 console.log(clientId + ' timed out.');
-              });
-              c.setKeepAlive(true, 0);
-              c.on('end', function() {
-                console.log(clientId + ' disconnected.');
-              });
-              c.on('data', function(buffer) {
-                 console.log(buffer);
-              });
-              
-            // IRC Logic
-              
-              c.write('hello\r\n');
-              c.pipe(c);
+            var session = new BIRCSession(c);
+            self.sessions.push(session);
 
             // New connection
             if (connectionListener && 'function' == typeof(connectionListener))
             {
-                connectionListener(this);
+                connectionListener(session);
             }
         };
         
